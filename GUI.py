f42c5b36d748865d571f1e5abe81bd547e0c154f
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2018-12-28 20:35:09 +0200

    Add a page widget and give it a vertical box layout

--- GUI.py
+++ GUI.py
@@ -10,7 +10,22 @@ class Window(QtWidgets.QMainWindow):
 		self.setGeometry(450, 200, 600, 400)
 		self.setWindowTitle("Take a break")
 		self.setWindowIcon(QtGui.QIcon("logo.png"))
-		
+
+		self.initUI()
+
+	def initUI(self):
+		# create a widget for the page and give it a layout
+		self.page = QtWidgets.QWidget()
+		self.layout = QtWidgets.QVBoxLayout()
+		self.layout.setAlignment(QtCore.Qt.AlignCenter)
+		self.page.setLayout(self.layout)
+		# set it as central widget
+		self.setCentralWidget(self.page)
+		# give the page a lightgray background
+		self.page.setStyleSheet("QWidget {background-color: lightgray}")
+
+
+
 
 def main():
 	app = QtWidgets.QApplication(sys.argv)
